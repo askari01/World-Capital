e1c31dbf9527b5f19fe4d6333c671773ac31c7c6
Farrukh Askari <farrukh.askari01@gmail.com>
2016-01-24 12:17:12 -0800

    Initial Commit

---
+++ World Capital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World Capital
+//
+//  Created by Syed Askari on 24/01/2016.
+//  Copyright © 2016 Syed Askari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
